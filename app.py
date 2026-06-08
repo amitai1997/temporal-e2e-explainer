ce86a6f8b6c103ebb8ddb5176193be75408ce10b
amitaipayzen <amitai@payzen.com>
2026-06-08 12:43:46 +0300

    trigger redeploy

--- app.py
+++ app.py
@@ -5,3 +5,4 @@ st.set_page_config(page_title="Durable E2E Tests on Temporal", layout="wide")
 
 html = pathlib.Path(__file__).parent / "flow.html"
 st.components.v1.html(html.read_text(), height=2200, scrolling=True)
+
